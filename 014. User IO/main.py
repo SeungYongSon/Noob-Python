ddb715ad54355129ada180c537bfc858bfbd1aa2
Sungyong Son <space010127@gmail.com>
2018-01-13 19:28:14 +0900

    2018. 01. 13 Study

---
+++ 014. User IO/main.py
@@ -0,0 +1,22 @@
+# 사용자 입력, 출력
+
+# 입력 input
+a = input()
+print(a)
+
+number = input("숫자 입력 :")
+print(number)
+
+# 출력 print
+
+# 큰따옴표로 둘러싸인 문자열은 + 연산과 같다.
+print("life" "is" "too short")
+print("life"+"is"+"too short")
+
+# 문자열 띄어쓰기는 콤마로 한다.
+print("life", "is", "too short")
+
+# 한 줄에 결과값 출력하기 end
+# end로 끝 문자를 지정할 수 있다.
+for i in range(10):
+    print(i, end=' ')  # end로 끝문자를 ' ' 으로 만들었다.
